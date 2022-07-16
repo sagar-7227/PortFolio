c42e0256c84ea07ef65add9393a1c5e82fcda227
Sagar Vashnav <sagarvashnav72427@gmail.com>
2022-07-16 21:44:17 +0530

    improved version

--- js/init.js
+++ js/init.js
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2018 Marketify
- * Author: Marketify
- * This file is made for CURRENT TEMPLATE
-*/
 
 
 jQuery(document).ready(function(){
@@ -538,7 +533,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Cloud Developer", "Backend Developer", "Competative Programmer"],
+			strings: ["Cloud Developer", "DevOps Developer", "Android Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
